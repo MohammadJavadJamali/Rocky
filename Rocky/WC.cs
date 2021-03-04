e8f303a9a163fb94ff9426fe1f3676fd0a33bd78
MohammadJavadJamali <67641670+MohammadJavadJamali@users.noreply.github.com>
2021-03-04 20:05:54 +0330

    Session get method
    use session get method  for receive  count of shopping cart

--- Rocky/WC.cs
+++ Rocky/WC.cs
@@ -8,5 +8,6 @@ namespace Rocky
     public static class WC
     {
         public static string ImagePath = @"\images\products\";
+        public static string sessionCart = "ShoppingCartSession";
     }
 }
